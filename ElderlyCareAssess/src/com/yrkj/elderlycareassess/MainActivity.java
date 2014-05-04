e2d3cdc0f589aff396be484b075b965d7c5be9d3
Koyoka <elevenrz@gmail.com>
2014-05-04 16:38:43 +0800

    ECA-测试actionBar

--- ElderlyCareAssess/src/com/yrkj/elderlycareassess/MainActivity.java
+++ ElderlyCareAssess/src/com/yrkj/elderlycareassess/MainActivity.java
@@ -1,15 +1,21 @@
 package com.yrkj.elderlycareassess;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
+import android.app.ActionBar;
+import android.app.ActionBar.OnNavigationListener;
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
+import android.widget.ArrayAdapter;
+import android.widget.SpinnerAdapter;
+import android.widget.TextView;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -19,16 +25,22 @@ public class MainActivity extends ActionBarActivity {
 		setContentView(R.layout.activity_main);
 
 		if (savedInstanceState == null) {
-			getSupportFragmentManager().beginTransaction()
+//			getSupportFragmentManager().beginTransaction()
+//			.add(arg0, arg1)
+//			getSupportFragmentManager().beginTransaction()
+			getFragmentManager().beginTransaction()
+//					.add(R.id.container, new PlaceholderFragment())
 					.add(R.id.container, new PlaceholderFragment()).commit();
 		}
+		
+		initActy();
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
+//		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
 
@@ -61,4 +73,56 @@ public class MainActivity extends ActionBarActivity {
 		}
 	}
 
+	public class StudentInfo extends Fragment
+	{
+	    // Fragment对应的标签，当Fragment依附于Activity时得到
+	    private String tag;
+
+	    @Override
+	    public void onAttach(Activity activity)
+	    {
+	        super.onAttach(activity);
+	        tag = getTag();
+	    }
+
+	    @Override
+	    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+	    {
+	        TextView textView = new TextView(getActivity());
+	        textView.setText(tag);
+	        return textView;
+	    }
+	}
+	
+	class DropDownListenser implements OnNavigationListener
+    {
+        // 得到和SpinnerAdapter里一致的字符数组
+        String[] listNames = getResources().getStringArray(R.array.action_list);
+
+        /* 当选择下拉菜单项的时候，将Activity中的内容置换为对应的Fragment */
+        public boolean onNavigationItemSelected(int itemPosition, long itemId)
+        {
+            // 生成自定的Fragment
+            StudentInfo student = new StudentInfo();
+            FragmentManager manager = getFragmentManager();
+//            FragmentManager manager = getSupportFragmentManager();
+            FragmentTransaction transaction = manager.beginTransaction();
+            // 将Activity中的内容替换成对应选择的Fragment
+            transaction.replace(R.id.container, student, listNames[itemPosition]);
+            transaction.commit();
+            return true;
+        }
+    }
+	
+	
+	private void initActy(){
+		SpinnerAdapter adapter = ArrayAdapter.createFromResource(this, R.array.action_list, android.R.layout.simple_spinner_dropdown_item);
+	
+		android.app.ActionBar actionBar = getActionBar();
+        // 将ActionBar的操作模型设置为NAVIGATION_MODE_LIST
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_LIST);
+        // 为ActionBar设置下拉菜单和监听器
+        actionBar.setListNavigationCallbacks(adapter, new DropDownListenser());
+	}
+	
 }
