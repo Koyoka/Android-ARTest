8e5a75755c85ba0e912950cc32c293d3f9c38986
Koyoka <elevenrz@gmail.com>
2015-02-10 22:50:47 +0800

    MWR 手机终端项目搭建 完成二维码扫描

---
+++ MWRMobile/src/com/yrkj/mwrmobile/MainActivity.java
@@ -0,0 +1,83 @@
+package com.yrkj.mwrmobile;
+
+import com.dtr.zxing.activity.CaptureActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.yrkj.mwrmobile.R;
+import com.yrkj.util.log.ToastUtil;
+
+public class MainActivity extends Activity implements OnClickListener {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		Intent intent = getIntent();
+		if(intent != null)
+		{
+			String result = intent.getStringExtra("result");
+			ToastUtil.show(this, result +"   eleven");
+		}
+		
+		initActy();
+	}
+	
+	private void initActy(){
+		
+		Button btn = (Button) findViewById(R.id.btnTest); 
+		btn.setOnClickListener(this);
+	}
+	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if(1==resultCode){
+			
+			int width = data.getExtras().getInt("width");//.getIntExtra("width", 0);
+			int height =  data.getExtras().getInt("height");
+			String result = data.getExtras().getString("result");
+			Toast.makeText(this, "result:" + result, Toast.LENGTH_LONG).show();
+		
+		}
+	}
+
+	@Override
+	public void onClick(View v) {
+
+		switch (v.getId()) {
+		case R.id.btnTest:
+			Intent intent = new Intent(MainActivity.this,CaptureActivity.class);
+			if(false){
+				intent.putExtra(CaptureActivity.INTENT_KEY_ResultType,
+						CaptureActivity.INTENT_VAL_ResultType_SetResult);
+				startActivityForResult(intent, 9);
+			}else{
+				
+				intent.putExtra(CaptureActivity.INTENT_KEY_ResultType,
+						CaptureActivity.INTENT_VAL_ResultType_NewActy);
+				intent.putExtra(CaptureActivity.INTENT_KEY_PKG,
+						"com.yrkj.mwrmobile");
+				intent.putExtra(CaptureActivity.INTENT_KEY_CLZName,
+						"com.yrkj.mwrmobile.MainActivity");
+				startActivity(intent);
+			}
+			
+			
+			break;
+		case R.id.result_image:
+			
+			break;
+		default:
+			break;
+		}
+	}
+	
+}
