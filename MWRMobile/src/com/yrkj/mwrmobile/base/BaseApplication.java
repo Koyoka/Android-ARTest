70988e7db1979a0d539b24c420b36ca8d954d0af
Koyoka <elevenrz@gmail.com>
2015-04-07 18:04:27 +0800

    MWR-MOBILE 添加百度定位lib

--- MWRMobile/src/com/yrkj/mwrmobile/base/BaseApplication.java
+++ MWRMobile/src/com/yrkj/mwrmobile/base/BaseApplication.java
@@ -2,15 +2,22 @@ package com.yrkj.mwrmobile.base;
 
 
 
+
+import com.baidu.location.BDLocation;
+import com.baidu.location.BDLocationListener;
+import com.baidu.location.LocationClient;
 import com.yrkj.util.log.DLog;
 
 import android.app.Application;
+import android.util.Log;
 
 public class BaseApplication extends Application {
 	
 	public static final String Service_URL = "http://192.168.1.201/Services/MWMobileWSHandler.ashx";
 	
 	private static BaseApplication mApplication = null;
+	public MyLocationListener mMyLocationListener;
+	public LocationClient mLocationClient;
 	
 	public static BaseApplication getInstance(){
 		return mApplication;
@@ -35,7 +42,7 @@ public class BaseApplication extends Application {
 			
 		}
 			
-		
+		mLocationClient.registerLocationListener(mMyLocationListener);
 		
 		
 //		String carCode, driver, driverName, inspector, inspectorName, wsCode;
@@ -50,6 +57,47 @@ public class BaseApplication extends Application {
 //		SysMng.saveTxnInfo(carCode,driver, driverName, inspector, inspectorName);
 //		SysMng.Launch(this);
 	}
+	
+	public class MyLocationListener implements BDLocationListener {
+
+		@Override
+		public void onReceiveLocation(BDLocation location) {
+			//Receive Location 
+			StringBuffer sb = new StringBuffer(256);
+			sb.append("time : ");
+			sb.append(location.getTime());
+			sb.append("\nerror code : ");
+			sb.append(location.getLocType());
+			sb.append("\nlatitude : ");
+			sb.append(location.getLatitude());
+			sb.append("\nlontitude : ");
+			sb.append(location.getLongitude());
+			sb.append("\nradius : ");
+			sb.append(location.getRadius());
+			if (location.getLocType() == BDLocation.TypeGpsLocation){
+				sb.append("\nspeed : ");
+				sb.append(location.getSpeed());
+				sb.append("\nsatellite : ");
+				sb.append(location.getSatelliteNumber());
+				sb.append("\ndirection : ");
+				sb.append("\naddr : ");
+				sb.append(location.getAddrStr());
+				sb.append(location.getDirection());
+			} else if (location.getLocType() == BDLocation.TypeNetWorkLocation){
+				sb.append("\naddr : ");
+				sb.append(location.getAddrStr());
+				//运营商信息
+				sb.append("\noperationers : ");
+				sb.append(location.getOperators());
+			}
+//			logMsg(sb.toString());
+			Log.i("BaiduLocationApiDem", sb.toString());
+		}
+
+
+	}
+	
+	
 	
 	
 }
